863258c2df78b6c3bb342ed8f08d3346fc415a48
Max Petersen <mglxp5@gmail.com>
2018-09-02 22:29:14 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,8 @@
 def oxford_comma(array)
   sentence = ""
-  if array.size == 2
+  if array.size == 1
+    sentence = array.first
+  elsif array.size == 2
     sentence = array[0] + " and " + array[1]
   else
   
